33a044b928bef982770b958b41f4bb112f69ac7b
Joshua Vial <joshua@enspiral.com>
2017-08-22 14:52:13 +1200

    register form working

--- client/actions/register.js
+++ client/actions/register.js
@@ -1,38 +1,15 @@
-import request from '../utils/api'
+import request from 'superagent'
 import {saveUserToken} from '../utils/auth'
 
-function registerUserRequest ({username, password}) {
-  request
-    .post('/api/v1/auth/register')
-    .set({
-      Accept: 'application/json',
-      Authorization: get('token')
-    })
-    .send({
-      username, password
-    })
-    .end((err, res) => {
-    })
+export function registerUserRequest ({username, password}) {
+  return (dispatch) => {
+    request
+      .post('/api/v1/auth/register')
+      .send({
+        username, password
+      })
+      .end((err, res) => {
+        console.log(res.body)
+      })
+  }
 }
-
-
-request
-  .get('/')
-  ,end((err, res) => {
-    callback(res.body)
-  })
-
-
-return request
-  .get('/')
-  .then((res) => return res)
-  .catch(err => )
-
-
-request('post', '/auth/register', {username, password})
-  .then((res) => {
-
-  })
-  .catch(err => {
-
-  })
--- client/components/RegisterForm.jsx
+++ client/components/RegisterForm.jsx
@@ -1,25 +1,43 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {registerUserRequest} from '../actions/register'
 
 
 class RegisterForm extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      username: '',
+      password: '',
+      confirm: ''
     }
    }
 
+   updateField(e) {
+     this.setState({
+       [e.target.name]: e.target.value
+     })
+   }
 
-
+   handleSubmit(e) {
+     e.preventDefault()
+     if (this.state.password != this.state.confirm) {
+        alert('wrong password, practice your typing')
+        this.setState({ password: '', confirm: '' })
+     } else {
+       const {username, password} = this.state
+      this.props.onSubmit({username, password})
+     }
+   }
 
    render() {
      return (
        <div>
          <h1>Register!</h1>
-         <form>
-           <input type="text" name="username" placeholder="username" />
-           <input type="password" name="hash" placeholder="password" />
+         <form onSubmit={this.handleSubmit.bind(this)}>
+           <input type="text" name="username" value={this.state.username} placeholder="username" onChange={this.updateField.bind(this)} />
+           <input type="password" name="password" value={this.state.password} onChange={this.updateField.bind(this)} />
+           <input type="password" name="confirm" value={this.state.confirm} onChange={this.updateField.bind(this)} />
            <input type="submit" value="Register" />
          </form>
        </div>
@@ -27,4 +45,10 @@ class RegisterForm extends React.Component {
    }
 }
 
-export default connect()(RegisterForm)
+function mapDispatchToProps(dispatch) {
+    return {
+      onSubmit: (creds) => {dispatch(registerUserRequest(creds))}
+    }
+}
+
+export default connect(undefined, mapDispatchToProps)(RegisterForm)
